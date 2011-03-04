ea4c51636dd5af0604cbb7daecb34cb0f606e757
Dave Knapik <daveknapik@gmail.com>
2011-03-04 10:27:54 -0600

    Added cucumber test for artist with no releases

--- features/step_definitions/artist.rb
+++ features/step_definitions/artist.rb
@@ -1,8 +1,13 @@
-Given /^there is one artist$/ do
+Given /^there is one artist with some releases$/ do
   Artist.destroy_all
   @artist = Factory.create(:artist, :name => "Fennesz", :bio => "Austrian guitar and laptop composer")
   @release = Factory.create(:release, :artist_name => "Fennesz", :title => "Endless Summer", :description => "Everyone loves Fennesz")
   @release = Factory.create(:release, :artist_name => "Fennesz", :title => "Venice", :description => "This one's good, too!")
+end
+
+Given /^there is one artist with no releases$/ do
+  Artist.destroy_all
+  @artist = Factory.create(:artist, :name => "Fennesz", :bio => "Austrian guitar and laptop composer")
 end
 
 When /^I follow the artist name$/ do
--- features/view-artist.feature
+++ features/view-artist.feature
@@ -4,10 +4,15 @@ Feature:
 	So that I can read all of his or her details
 	
 Scenario:
-	Given there is one artist
+	Given there is one artist with some releases
 	Given I am on the release list page
 	When I follow the artist name
 	Then I should be on the artist page
 	And I should see the artist name
 	And I should see the artist bio
 	And I should see the artists releases
+	
+Scenario:
+	Given there is one artist with no releases
+	Given I am on the artist page
+	Then I should see "This artist has no releases"
